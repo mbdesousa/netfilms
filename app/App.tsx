fe4aa2d298ac9da398b1dd24a4ec9dbf5b279daa
Mateus <mateusbdesousa@gmail.com>
2022-06-08 14:40:38 -0300

    general configuration

---
+++ app/App.tsx
@@ -0,0 +1,45 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import { View } from "react-native";
+
+const HomeScreen = (): JSX.Element => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// export default function App() {
+//   const isLoadingComplete = useCachedResources();
+//   const colorScheme = useColorScheme();
+
+//   if (!isLoadingComplete) {
+//     return null;
+//   } else {
+//     return (
+//       <SafeAreaProvider>
+//         <Navigation colorScheme={colorScheme} />
+//         <StatusBar hidden />
+
+
+
+//       </SafeAreaProvider>
+//     );
+//   }
+// }
+
+export default App;
